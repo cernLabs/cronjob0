0d7ac9879e7046058260930be9ea5963ec857667
cernLabs <mikpen.p@gmail.com>
2023-06-12 22:31:00 -0700

    added  to the repo at 22:31:00

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cronjob0
+This is a bash command that creates a file daily and uploads to a github repo
+Use 
+chmod u+x filegen.sh to make it executeable 
---
+++ filegen.sh
@@ -0,0 +1,11 @@
+!# /bin/bash
+
+#lets generate file name
+filename=$(date +%Y-%m-%d).txt
+
+#put something into the file
+date >> /days/$filename
+
+# run git cmds
+git add . 
+git commit -m "added $filname to the repo at $(date +%H:%M:%S)"
